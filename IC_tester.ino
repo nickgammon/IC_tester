4f015339175c5197bc7b7acee001d7494f374ee4
Nick Gammon <nick@gammon.com.au>
2022-01-07 10:37:07 +1100

    Moved test data to separate file

--- IC_tester.ino
+++ IC_tester.ino
@@ -37,8 +37,8 @@ int chipPins16 [16] = {10, 9, 8, 7, 6, 5, 4, 3, 11, 12, A5, A4, A3, A2, A1, A0};
 int * pinsMap;      // will point to either of the above
 byte numberOfPins;  // how many pins it has
 
-// optional LED - flashes to show we are testing
-int INDICATOR_LED = 2;
+byte PIN7_GROUND = 2;
+byte PIN8_GROUND = 13;
 
 // how much serial data from the user we expect before a newline
 const unsigned int MAX_INPUT = 16;
@@ -52,49 +52,9 @@ char chipName [MAX_INPUT + 1];
 // pointer to current chip info (starts with number of pins)
 const char * chipInfo = NULL;
 
-// testing data (multi-line raw string)
 
 /*
-   CHIP DATA
-
-   $xxxx = chip name - used for searching (Note: 74LS00, for example, is here as 7400)
-   xx    = number of pins (currently 14 and 16 supported)
-
-   Then one or more lines of 14/16 characters indicating the test condition
-   and expected results.
-
-   Test data terminated by "$" for the next chip, or "&" for the end of the list.
-   
-   V = Vcc (ie. +5V)
-   G = Gnd
-
-   0/1 = Set this pin to LOW/HIGH (output from sketch, input to DuT)
-   L/H = Expect LOW/HIGH from device (input into sketch, output from DuT)
-   X =   Ignore (set pin to high impedance and do not test it)
-
-   Clock pulses are for toggling a pin prior to the test (eg. for serial shift registers)
-   C =   Pulse clock (normally LOW, pulse to HIGH, and back to LOW)
-   c =   Pulse clock (normally HIGH, pulse to LOW, and back to HIGH)
-
-
-  NB: Changed chip 74193 to be different from the file downloaded above.
-      According to the datasheet, the clock lines CPD and CPU (clock down and clock up)
-      should be high while the other one is pulsed. Thus, they need to be normally high
-      (and pulsed low) to avoid a spurious extra clock pulse. However the chip increments
-      or decrements the counter on the rising edge. This still works done this way, however
-      the results are not what are shown in the file (link below). To pulse HIGH->LOW rather
-      than LOW->HIGH I introduced the lower-case code "c" for this action.
-
-  Copy/paste data for desired chips from the larger test file available from:
-
-     http://gammon.com.au/Arduino/test_16_full_0004.dat
-     
-  (Not all will fit into memory).
-
-  No warranty is given as to the correctness of the file or test data. I have tested on a few
-  chips I have to hand and the test data works OK on them. Specifically:
-    7404, 7408, 7430, 74163, 74173, 74193, 4030
-
+ 
   WARNING:
 
     Because Gnd and Vcc are supplied through the (recommended) 330 ohm current-limiting resistors
@@ -102,190 +62,19 @@ const char * chipInfo = NULL;
     connect Vcc for the DuT to +5V on the Arduino, and Gnd for the DuT to Gnd on the Arduino.
     For many chips Vcc is the highest pin (14 or 16 as the case may be) and Gnd will be pin 7
     (for 14-pin chips) or pin 8 (for 16-pin chips).
-    
+
+  WORK-AROUND:
+  
+    The code below attempts to at least connect Gnd directly to pins 7 or 8 (depending on the chip)
+    via an additional couple of wires that bypasses the current-limiting resistor, see documentation
+    for more details.
+
+    Most (if not all) chips in the test file have ground on pin 7 (for 14-pin chips) and pin 8
+    (for 16-pin chips).
     
  */
-const char chipData[] PROGMEM = R"(0001
-$4013
-14
-LHC100G001CHLV
-HLC001G100CLHV
-LHC000G000CHLV
-HLC010G010CLHV
-$4066
-14
-0HH000G0HH000V
-1HH100G1HH100V
-0LL011G0LL011V
-1HH111G1HH111V
-$4075
-14
-00000LG0LL000V
-00110HG1HH100V
-10010HG0HH010V
-10110HG1HH110V
-11001HG0HH001V
-01101HG1HH101V
-11011HG0HH011V
-11111HG1HH100V
-$4081
-14
-00LH11G11HL00V
-10LL10G10LL10V
-01LL01G01LL01V
-11HL00G00LH11V
-$4093
-14
-00HH00G00HH00V
-10HH10G10HH10V
-01HH01G01HH01V
-11LL11G11LL11V
-$7400
-14
-00H00HGH00H00V
-10H10HGH10H10V
-01H01HGH01H01V
-11L11LGL11L11V
-$7401
-14
-H00H00G00H00HV
-H10H10G10H10HV
-H01H01G01H01HV
-L11L11G11L11LV
-$7402
-14
-H00H00G00H00HV
-L10L10G10L10LV
-L01L01G01L01LV
-L11L11G11L11LV
-$7403
-14
-00H00HGH00H00V
-10H10HGH10H10V
-01H01HGH01H01V
-11L11LGL11L11V
-$7404
-14
-0H0H0HGH0H0H0V
-1L1L1LGL1L1L1V
-$7405
-14
-0H0H0HGH0H0H0V
-1L1L1LGL1L1L1V
-$7406
-14
-0H0H0HGH0H0H0V
-1L1L1LGL1L1L1V
-$7407
-14
-1H1H1HGH1H1H1V
-0L0L0LGL0L0L0V
-$7408
-14
-00L00LGL00L00V
-01L01LGL01L01V
-10L10LGL10L10V
-11H11HGH11H11V
-$7409
-14
-11H11HGH11H11V
-10L10LGL10L10V
-01L01LGL01L01V
-00L00LGL00L00V
-$7410
-14
-00000HGH000H0V
-00100HGH100H1V
-10010HGH010H0V
-10110HGH110H1V
-01001HGH001H0V
-01101HGH101H1V
-11011HGH011H0V
-11111LGL111L1V
-$7430
-14
-111111GL00110V
-011111GH00110V
-101111GH00110V
-110111GH00110V
-111011GH00110V
-111101GH00110V
-111110GH00110V
-111111GH00010V
-111111GH00100V
-100000GH00000V
-$74193
-16
-1HH00HHG110XX01V
-1HLc1HHG111XX01V
-1LHc1HHG111XX01V
-1LLc1HHG111XX01V
-1LH1cHHG111XX01V
-1HL1cHHG111XX01V
-1HH1cHHG111XX01V
-1LLc1LLG111XX11V
-$74173
-16
-00LLLLCG0000001V
-00LLLLCG1111111V
-00LLLLCG0000000V
-00LLLLCG1111110V
-00LLLLCG1000000V
-00LLLLCG1111110V
-00LLLLCG0100000V
-00LLLLCG1111110V
-00LLLLCG0000000V
-$4030
-14
-10HH10G10HH10V
-01HH01G01HH01V
-00LL00G00LL00V
-11LL11G11LL11V
-$4031
-16
-0CXXXHLGX0XXXX1V
-0CXXXLHGX0XXXX0V
-1CXXXHLGX1XXXX0V
-0CXXXLHGX1XXXX0V
-$74161
-16 
-0100000G00LLLLLV
-0100001G11LLLLLV
-1C00001G10LLLLLV
-1C00000G11LLLLLV
-1C00000G10LLLLLV
-1C00000G00LLLLLV
-1C00111G01HHLLLV
-1C00001G11HHLHLV
-1C00001G11HHHLLV
-1C00001G11HHHHHV
-1C00001G11LLLLLV
-1C00001G11LLLHLV
-1100001G11LLHLLV
-1000001G11LLHLLV
-0000001G11LLLLLV
-1C00001G11XXXXXV
-$74162
-16 
-0100000G00XXXXXV
-0C00000G00LLLLLV
-0C00000G00LLLLLV
-0C10000G11LLLLLV
-1C00001G10LLLLLV
-1C00000G11LLLLLV
-0C00000G11LLLLLV
-1C11100G00LHHHLV
-1C00111G11HLLLLV
-1C00001G11HLLHHV
-1C00001G11LLLLLV
-1C00001G11LLLHLV
-1C00001G11LLHLLV
-1100001G11LLHHLV
-0100001G11LLHHLV
-0000001G11LLHHLV
-0100001G11LLLLLV
-1C00001G11XXXXXV
-&)";
+
+#include "chipData.h"
 
 
 void setup()
@@ -295,8 +84,6 @@ void setup()
   Serial.println (F("IC tester - written by Nick Gammon."));
   Serial.println (F("Version 1.0. Date: 6 January 2022."));
   Serial.println (F("Enter L to list known chips."));
-  pinMode (INDICATOR_LED, OUTPUT);
-  digitalWrite (INDICATOR_LED, LOW);
   } // end of setup
 
 // For checking your wiring, put LEDs into each pin on the chip socket
@@ -412,13 +199,10 @@ int singleTest (const char * testLine, const int testNumber)
   byte i;
   int failed = 0;
 
-  digitalWrite (INDICATOR_LED, HIGH);
-
   Serial.print (F("Testing case "));
   showNumber (testNumber);
   Serial.print (F(": "));
   Serial.print (testLine);
-
 
   // set ground to LOW
   for (i = 0; i < numberOfPins; i++)
@@ -426,6 +210,18 @@ int singleTest (const char * testLine, const int testNumber)
     {
       digitalWrite (pinsMap [i], LOW);
       pinMode (pinsMap [i], OUTPUT);
+
+      // see if ground override possible
+      if (i == 6)  // that is pin 7 since this is zero-relative
+        {
+        digitalWrite (PIN7_GROUND, LOW);
+        pinMode (PIN7_GROUND, OUTPUT);
+        }
+      else if (i == 7)  // that is pin 8 since this is zero-relative
+        {
+        digitalWrite (PIN8_GROUND, LOW);
+        pinMode (PIN8_GROUND, OUTPUT);
+        }
     } // end of for each pin
 
   // set power to HIGH
@@ -547,8 +343,6 @@ int singleTest (const char * testLine, const int testNumber)
   else
     Serial.println (F(" ok"));
 
-  digitalWrite (INDICATOR_LED, LOW);
-
   return failed;
   } // end of singleTest
 
@@ -601,6 +395,9 @@ void testChip ()
   // set all pins to input initially
   for (i = 0; i < numberOfPins; i++)
     pinMode (pinsMap [i], INPUT);
+
+  pinMode (PIN7_GROUND, INPUT);
+  pinMode (PIN8_GROUND, INPUT);
 
   int testNumber = 0;
   
@@ -665,6 +462,9 @@ void testChip ()
   // set all pins to input afterwards
   for (i = 0; i < numberOfPins; i++)
     pinMode (pinsMap [i], INPUT);
+
+  pinMode (PIN7_GROUND, INPUT);
+  pinMode (PIN8_GROUND, INPUT);
 
   Serial.print (F("Done - "));
   if (failures)
